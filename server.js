328569e8684108952d97359cebc1e3653c2e7468
Angel-Huichapa <146030239+Angel-Huichapa@users.noreply.github.com>
2024-04-11 17:39:23 -0600

    changed the port to work in heroku

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.listen(3000, () => {
+app.listen(process.env.Port || 3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
